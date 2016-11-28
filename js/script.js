d82b920f3cbf894709a9d35fb96499603183c797
Hemmar44 <hedrzak.marcin@gmail.com>
2016-11-28 22:29:59 +0100

    prace trwają

--- js/script.js
+++ js/script.js
@@ -2,5 +2,22 @@ $(function(){
 	$(".knowledge").on("click", function(){
 		//alert("cv");
 		$(this).parent("li").find("ul").slideToggle();
+		var secName = $(this).closest("section");
+		var secPic = $(this).closest("main").find("section").first();
+		var secNameHeight = secName.height();
+		var secPicHeight = secPic.height();
+		if((secPicHeight)<(secNameHeight)){
+			secPic.animate({height:secNameHeight})
+		}
+		else{
+			secPic.animate({height:secPicHeight})
+		}
+
+		
+		
+		
+		
+		alert("first " + secPicHeight);
+		alert("second " + secNameHeight);
 	});
 });
