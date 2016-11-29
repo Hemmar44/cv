396ed02e283c0d94d3d63e7ede0724c5d512b048
Hemmar44 <hedrzak.marcin@gmail.com>
2016-11-29 09:50:34 +0100

    wysokoscgit add .!

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 $(function(){
+	var secNameHeight = 0;
+	var secPicHeight = 0;
 	$(".knowledge").on("click", function(){
 		//alert("cv");
 		$(this).parent("li").find("ul").slideToggle();
-		var secName = $(this).closest("section");
-		var secPic = $(this).closest("main").find("section").first();
-		var secNameHeight = secName.height();
-		var secPicHeight = secPic.height();
-		if((secPicHeight)<(secNameHeight)){
-			secPic.animate({height:secNameHeight})
-		}
-		else{
-			secPic.animate({height:secPicHeight})
-		}
-
-		
-		
-		
-		
-		alert("first " + secPicHeight);
-		alert("second " + secNameHeight);
 	});
+	
+	//if(secPicHeight>=secNameHeight) {
+	//		$("main").css("background-color","white");
+	//	}
+	
+
 });
